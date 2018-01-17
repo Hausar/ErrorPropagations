9923d751dd048ad0b244b1f940e8b822b85477e3
Helene Ausar <helene.ausar@cern.ch>
2018-01-17 13:13:36 +0100

    changes to Rebin chi422, chi532

--- Rebin_Plots.cxx
+++ Rebin_Plots.cxx
@@ -63,47 +63,84 @@ void Rebin_Plots()
     //********************************************************************************************************************
     //.. V5,32: 
     //TFile *file = TFile::Open("/Users/Helena/Desktop/Helen_simpletask/ErrorPropagations/V5_Linear_NonLinear_Response_WOGap.root", "READ");
-    TFile *file = TFile::Open("/Users/Helena/Desktop/Helen_simpletask/ErrorPropagations/V5_Linear_NonLinear_Response_Gap02.root", "READ");
+    // TFile *file = TFile::Open("/Users/Helena/Desktop/Helen_simpletask/ErrorPropagations/V5_Linear_NonLinear_Response_Gap02.root", "READ");
+    
+    // //..TProfile for V5:
+    // TH1D *hC22_Rebin = new TH1D("hC22_Rebin", " v_{5}{2}, |#Delta #eta| > 0.2", 20, 0, 200); //this is just define a histogram which will fill the data.
+    // hC22_Rebin->Sumw2();
+    // //hC22_Rebin->Draw();
+
+    // TH1D *hCV532_Rebin = new TH1D("hCV532_Rebin", "Non-Linear Response, v_{5,32} |#Delta #eta| > 0.2", 20, 0, 200); //this is just define a histogram which will fill the data of V422.
+    // hCV532_Rebin->Sumw2();
+
+
+    // TH1D *hCLinear_Rebin = new TH1D("hCLinear_Rebin", " The Linear Response v_{5}^{L} ", 20, 0, 200); //this is just define a histogram which will fill the data.
+    // hCLinear_Rebin->Sumw2();
+
+    // TH1D *hC22 = (TH1D*)file->Get("hC22");
+    // TH1D *hCV532 = (TH1D*)file->Get("hCV532");
+    // TH1D *hCLinear = (TH1D*)file->Get("hCLinear");
+
+    // //hC22->Draw("same");
+    // // hCV532->Draw("same");
+    // // hCLinear->Draw("same");
+    // int binning = 10;
+    // double errorNum;
+    // double sumNum = 0;
+    // double sumDenom = 1;
+    // double content = 0;
+
+    // if(binning == 10)RebinAll(hC22, hC22_Rebin, 10);
+    // if(!hC22) fPrint("Warning there is no histogram");
+    // hC22_Rebin->GetXaxis()->SetRangeUser(10, 110);
+    // //hC22_Rebin->Draw("same");
+
+    // if(binning == 10)RebinAll(hCV532, hCV532_Rebin, 10);
+    // if(!hCV532) fPrint("Warning there is no histogram");
+    // hCV532_Rebin->GetXaxis()->SetRangeUser(10, 110);
+    // //hCV532_Rebin->Draw("same");
+
+    // if(binning == 10)RebinAll(hCLinear, hCLinear_Rebin, 10);
+    // if(!hCLinear) fPrint("Warning there is no histogram");
+    // hCLinear_Rebin->GetXaxis()->SetRangeUser(10, 110);
+    // //hCLinear_Rebin->Draw("same");
+
+    //********************************************************************************************************************
+    //.. Chi_4,22 and Chi_5,32: 
+    TFile *file = TFile::Open("/Users/Helena/Desktop/Helen_simpletask/ErrorPropagations/Chi422_Chi532_Gap02.root", "READ");
+    // TFile *file = TFile::Open("/Users/Helena/Desktop/Helen_simpletask/ErrorPropagations/V5_Linear_NonLinear_Response_Gap02.root", "READ");
     
     //..TProfile for V5:
-    TH1D *hC22_Rebin = new TH1D("hC22_Rebin", " v_{5}{2}, |#Delta #eta| > 0.2", 20, 0, 200); //this is just define a histogram which will fill the data.
-    hC22_Rebin->Sumw2();
-    //hC22_Rebin->Draw();
+    TH1D *hChi422_Rebin = new TH1D("hChi422_Rebin", " #chi_{4,22}, |#Delta #eta| > 0.2", 20, 0, 200); //this is just define a histogram which will fill the data.
+    hChi422_Rebin->Sumw2();
+    //hChi422_Rebin->Draw();
 
-    TH1D *hCV532_Rebin = new TH1D("hCV532_Rebin", "Non-Linear Response, v_{5,32} |#Delta #eta| > 0.2", 20, 0, 200); //this is just define a histogram which will fill the data of V422.
-    hCV532_Rebin->Sumw2();
+    TH1D *hChi532_Rebin = new TH1D("hChi532_Rebin", " #chi_{5,32} |#Delta #eta| > 0.2", 20, 0, 200); //this is just define a histogram which will fill the data of hChi532.
+    hChi532_Rebin->Sumw2();
 
+    TH1D *hChi422 = (TH1D*)file->Get("hChi422");
+    TH1D *hChi532 = (TH1D*)file->Get("hChi532");
 
-    TH1D *hCLinear_Rebin = new TH1D("hCLinear_Rebin", " The Linear Response v_{5}^{L} ", 20, 0, 200); //this is just define a histogram which will fill the data.
-    hCLinear_Rebin->Sumw2();
-
-    TH1D *hC22 = (TH1D*)file->Get("hC22");
-    TH1D *hCV532 = (TH1D*)file->Get("hCV532");
-    TH1D *hCLinear = (TH1D*)file->Get("hCLinear");
-
-    //hC22->Draw("same");
-    // hCV532->Draw("same");
-    // hCLinear->Draw("same");
+    // hChi422->Draw("same"); 
+    // hChi532->Draw("same");
+    
     int binning = 10;
     double errorNum;
     double sumNum = 0;
     double sumDenom = 1;
     double content = 0;
 
-    if(binning == 10)RebinAll(hC22, hC22_Rebin, 10);
-    if(!hC22) fPrint("Warning there is no histogram");
-    hC22_Rebin->GetXaxis()->SetRangeUser(10, 110);
-    //hC22_Rebin->Draw("same");
+    if(binning == 10)RebinAll(hChi422, hChi422_Rebin, 10);
+    if(!hChi422) fPrint("Warning there is no histogram");
+    hChi422_Rebin->GetXaxis()->SetRangeUser(10, 140);
+    hChi422_Rebin->GetYaxis()->SetRangeUser(0, 10);
+    //hChi422_Rebin->Draw("same");
 
-    if(binning == 10)RebinAll(hCV532, hCV532_Rebin, 10);
-    if(!hCV532) fPrint("Warning there is no histogram");
-    hCV532_Rebin->GetXaxis()->SetRangeUser(10, 110);
-    //hCV532_Rebin->Draw("same");
-
-    if(binning == 10)RebinAll(hCLinear, hCLinear_Rebin, 10);
-    if(!hCLinear) fPrint("Warning there is no histogram");
-    hCLinear_Rebin->GetXaxis()->SetRangeUser(10, 110);
-    //hCLinear_Rebin->Draw("same");
+    if(binning == 10)RebinAll(hChi532, hChi532_Rebin, 10);
+    if(!hChi532) fPrint("Warning there is no histogram");
+    hChi532_Rebin->GetXaxis()->SetRangeUser(10, 140);
+    hChi532_Rebin->GetYaxis()->SetRangeUser(0, 10);
+    //hChi532_Rebin->Draw("same");
 
     // *****************************************************************************
     // ..Plot the results:        
@@ -115,13 +152,13 @@ void Rebin_Plots()
     // hCLinear_Rebin->Draw();
     // c->BuildLegend();
 
-    hCLinear_Rebin->SetMarkerStyle(21);
-    hCLinear_Rebin->SetMarkerColorAlpha(kTeal, 4.5);
-    hCLinear_Rebin->SetXTitle("# of tracks");
-    hCLinear_Rebin->SetYTitle("v_{5}^{L}");
-    hCLinear_Rebin->SetMarkerSize(2.);
-    hCLinear_Rebin->Draw();
-    c->BuildLegend();
+    // hCLinear_Rebin->SetMarkerStyle(21);
+    // hCLinear_Rebin->SetMarkerColorAlpha(kTeal, 4.5);
+    // hCLinear_Rebin->SetXTitle("# of tracks");
+    // hCLinear_Rebin->SetYTitle("v_{5}^{L}");
+    // hCLinear_Rebin->SetMarkerSize(2.);
+    // hCLinear_Rebin->Draw();
+    // c->BuildLegend();
 
     // hCV422_Rebin->SetMarkerStyle(34);
     // hCV422_Rebin->SetMarkerColorAlpha(kBlue, 0.45);
@@ -148,8 +185,85 @@ void Rebin_Plots()
     // hC22_Rebin->Draw("same");
     // c->BuildLegend();
 
+    //**************************************************************************
+    //..Plot Chi422: 
+    hChi422_Rebin->GetYaxis()->SetRangeUser(0., 4.);
+    hChi422_Rebin->GetXaxis()->SetRangeUser(0., 140);
+    hChi422_Rebin->SetMarkerSize(1.);
+    hChi422_Rebin->Draw("same");
+    hChi422_Rebin->SetTitle("Non-Linear coefficient #chi_{4,22} of v_{4,22} for pPb at 5.02 TeV");
+    hChi422_Rebin->SetMarkerStyle(kFullTriangleUp); 
+    hChi422_Rebin->SetMarkerColor(kRed+1);
+    hChi422_Rebin->GetYaxis()->SetTitleOffset(1.4);
+    //hChi422_Rebin->GetYaxis()->CenterTitle(true);
+    hChi422_Rebin->GetYaxis()->SetTitle("#chi_{4,22}");
+
+    hChi422_Rebin->GetXaxis()->SetTitleOffset(1.4);
+    //hChi422_Rebin->GetXaxis()->CenterTitle(true);
+    gStyle->SetTitleFontSize(0.2);
+    hChi422_Rebin->GetXaxis()->SetTitle("N_{Ch}(|#Delta#eta| < 0.8)");
+    hChi422_Rebin->SetFillColor(kRed-9);
+    c->Update();
+    
+    TLatex latex;
+    latex.SetTextSize(0.035);
+    latex.SetTextAlign(13);  //align at top
+    latex.DrawLatex(10,3.5,"#sqrt{s_{NN}} = 5.02 TeV");
+
+    latex.SetTextAlign(13);  //centered
+    latex.DrawLatex(10,3.3,"|#Delta #eta| < 0.8");
+
+    latex.SetTextAlign(13);  //centered
+    latex.DrawLatex(10,3.1," 0.2 < p_{T} < 3.0");
+
+    TLegend *leg = new TLegend(0.4, 0.5, 0.55, 0.55); 
+    leg->AddEntry(hChi422_Rebin, "#chi_{4,22}", "fp");
+    leg->Draw("same");
+    
+    leg->SetBorderSize(0);
+    gStyle->SetTitleFontSize(0.2);
+
+    //************************************************
+    //..Plot Chi532
+    hChi532_Rebin->GetYaxis()->SetRangeUser(0., 10.);
+    hChi532_Rebin->GetXaxis()->SetRangeUser(0., 140);
+    hChi532_Rebin->SetMarkerSize(1.);
+    hChi532_Rebin->Draw("same");
+    hChi532_Rebin->SetTitle("Non-Linear coefficient #chi_{5,32} of v_{5,32} for pPb at 5.02 TeV");
+    hChi532_Rebin->SetMarkerStyle(kFullTriangleUp); 
+    hChi532_Rebin->SetMarkerColor(kBlue+1);
+    hChi532_Rebin->GetYaxis()->SetTitleOffset(1.4);
+    //hChi532_Rebin->GetYaxis()->CenterTitle(true);
+    hChi532_Rebin->GetYaxis()->SetTitle("#chi_{5,32}");
+
+    hChi532_Rebin->GetXaxis()->SetTitleOffset(1.4);
+    //hChi532_Rebin->GetXaxis()->CenterTitle(true);
+    gStyle->SetTitleFontSize(0.2);
+    hChi532_Rebin->GetXaxis()->SetTitle("N_{Ch}(|#Delta#eta| < 0.8)");
+    hChi532_Rebin->SetFillColor(kBlue-9);
+    c->Update();
+    
+    TLatex latex;
+    latex.SetTextSize(0.035);
+    latex.SetTextAlign(13);  //align at top
+    latex.DrawLatex(10,3.5,"#sqrt{s_{NN}} = 5.02 TeV");
+
+    latex.SetTextAlign(13);  //centered
+    latex.DrawLatex(10,3.3,"|#Delta #eta| < 0.8");
+
+    latex.SetTextAlign(13);  //centered
+    latex.DrawLatex(10,3.1," 0.2 < p_{T} < 3.0");
+
+    TLegend *leg = new TLegend(0.4, 0.5, 0.55, 0.55); 
+    leg->AddEntry(hChi532_Rebin, "#chi_{5,32}", "fp");
+    leg->Draw("same");
+    
+    leg->SetBorderSize(0);
+    gStyle->SetTitleFontSize(0.2);
+
     //***************************************************************************
     //..Writting to the Root.file:
+    //..V4,22
     // TFile* fileOutput = new TFile("Rebin_Plots_V4_Linear_NonLinear_Response_Gap02.root","RECREATE");
     // if(!fileOutput) return;
     // fileOutput->cd();
@@ -168,6 +282,15 @@ void Rebin_Plots()
     // hCLinear_Rebin->Write();
     // fileOutput->Close();
     // return;
+
+    //..Chi4,22 and Chi5,32
+    TFile* fileOutput = new TFile("Rebin_Plots_Chi422_Chi532_Gap02.root","RECREATE");
+    if(!fileOutput) return;
+    fileOutput->cd();
+    hChi422_Rebin->Write();
+    hChi532_Rebin->Write();
+    fileOutput->Close();
+    return;
 }
 
 //..Define Rebin Function
