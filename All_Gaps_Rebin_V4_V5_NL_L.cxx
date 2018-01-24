8297a1bde57ba57022f0eecbed3df19077c3e7ea
Helene Ausar <helene.ausar@cern.ch>
2018-01-24 13:40:50 +0100

    All Gaps for V4, will successfully rebinned :-)

--- All_Gaps_Rebin_V4_V5_NL_L.cxx
+++ All_Gaps_Rebin_V4_V5_NL_L.cxx
@@ -1,6 +1,5 @@
 //..The macro for Rebin the V4_NL, V4_L, V4{2} and 
 //..The macro for Rebin the V5_NL, V5_L, V5{2}
-
 //Root file:
 #include <TFile.h>
 #include <TMath.h>
@@ -18,55 +17,102 @@ void All_Gaps_Rebin_V4_V5_NL_L()
     c->SetFillColor(10);
     c->cd();
 
+    //..Create TFile.root for all Rebin histograms:
+
+    TFile* fileOutput = new TFile("/Users/Helena/Desktop/Helen_simpletask/ErrorPropagations/AllGaps_Linear_NL_V4_V5/Rebin_V4_AllGaps/Rebin_V4_Linear_NL_histograms.root","RECREATE");
+
     //**************************************************************************
     //..V4,22 
 
-    TH1D *hC22_Rebin = new TH1D("hC22_Rebin", "v_{4}{2}, |#Delta #eta| > 0.2", 10, 0, 200); //this is just define a histogram which will fill the data.
-    hC22_Rebin->Sumw2();
-    //hC22_Rebin->Draw();
-
-    TH1D *hCV422_Rebin = new TH1D("hCV422_Rebin", "Non-Linear Response, v_{4,22}", 10, 0, 200); //this is just define a histogram which will fill the data of V422.
-    hCV422_Rebin->Sumw2();
-
-    TH1D *hCLinear_Rebin = new TH1D("hCLinear_Rebin", " The Linear Response v4^{L} ", 10, 0, 200); //this is just define a histogram which will fill the data.
-    hCLinear_Rebin->Sumw2();
-
-    //..Histogram for bin length= 40
-    //TH1D *hC22_Rebin = new TH1D("hC22_Rebin", "v_{4}{2}, |#Delta #eta| > 0.2", 5, 0, 200); //this is just define a histogram which will fill the data.
-    //hC22_Rebin->Sumw2();
-
     // //..Open Root.File:
-    TFile *file = TFile::Open("/Users/Helena/Desktop/Helen_simpletask/ErrorPropagations/AllGaps_Linear_NL_V4_V5/V4_AllGaps/Gap0_V4_Linear_NonLinear_Response.root", "READ");
-	
+    int EtaGap[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+
+    for(int i=0; i <9; i++) {
+        TString fPath (TString::Format("/Users/Helena/Desktop/Helen_simpletask/ErrorPropagations/AllGaps_Linear_NL_V4_V5/V4_AllGaps/Gap0%0d_V4_Linear_NonLinear_Response.root", EtaGap[i]));
+
+        TFile* file = TFile::Open(fPath, "READ");
+        if(!file) return;
+
+        TH1D* hC22 = (TH1D*)file->Get("hC22");
+        if(!hC22) return;
+        //hC22->Draw();
+
+        TH1D* hC22_Rebin = new TH1D(Form("hC22_Rebin_Gap0%0d", EtaGap[i]),"v_{4}{2}, 2-Particle correlations; #N_ch; #v_{4}{2}", 10, 0, 200);
+		hC22_Rebin->Sumw2();
+        //hC22_Rebin->Draw();
+
+        TH1D* hCV422_Rebin = new TH1D(Form("hCV422_Rebin_Gap0%0d", EtaGap[i]),"v_{4,22} Non-Linear Response; #N_ch; #v_{4,22}", 10, 0, 200);
+		hCV422_Rebin->Sumw2();
+
+        TH1D* hCLinear_Rebin = new TH1D(Form("hCLinear_Rebin_Gap0%0d", EtaGap[i]),"v_{4}^{L} Linear Response; #N_ch; #v_{4}^{L}", 10, 0, 200);
+		hCLinear_Rebin->Sumw2();
+
+        Printf("the merge directory exist:");
+        cout << "EtaGap: " << EtaGap[i] << endl;
+        cout << "i : " << i << endl; 
+
+        int binning = 20;
+        double errorNum;
+        double sumNum = 0;
+        double sumDenom = 1;
+        double content = 0;
+        
+        if(binning == 20)RebinAll(hC22, hC22_Rebin, 20);
+        if(!hC22) fPrint("Warning there is no histogram");
+        //hC22_Rebin->GetXaxis()->SetRangeUser(10, 110);
+        //hC22_Rebin->Draw("same");
+
+        if(binning == 20)RebinAll(hCV422, hCV422_Rebin, 20);
+        if(!hCV422) fPrint("Warning there is no histogram");
+        //hCV422_Rebin->GetXaxis()->SetRangeUser(10, 110);
+        //hCV422_Rebin->Draw("same");
+
+        if(binning == 20)RebinAll(hCLinear, hCLinear_Rebin, 20);
+        if(!hCLinear) fPrint("Warning there is no histogram");
+        //hCLinear_Rebin->GetXaxis()->SetRangeUser(10, 110);
+        //hCLinear_Rebin->Draw("same");
+
+        if(!fileOutput) return;
+        fileOutput->cd();
+        hC22_Rebin->Write();
+        hCV422_Rebin->Write();
+        hCLinear_Rebin->Write();
+        fileOutput->Write(Form("hC22_Rebin_Gap0%0d", EtaGap[i]));
+        fileOutput->Write(Form("hCV422_Rebin_Gap0%0d", EtaGap[i]));
+        fileOutput->Write(Form("hCLinear_Rebin_Gap0%0d", EtaGap[i]));
+    }
+
+    //*********************************************************************************************************************
+
     // TFile *file = TFile::Open("/Users/Helena/Desktop/Helen_simpletask/ErrorPropagations/V4_Linear_NonLinear_Response_Gap02.root", "READ");
 
-    TH1D *hC22 = (TH1D*)file->Get("hC22");
-    TH1D *hCV422 = (TH1D*)file->Get("hCV422");
-    TH1D *hCLinear = (TH1D*)file->Get("hCLinear");
+    // TH1D *hC22 = (TH1D*)file->Get("hC22");
+    // TH1D *hCV422 = (TH1D*)file->Get("hCV422");
+    // TH1D *hCLinear = (TH1D*)file->Get("hCLinear");
 
-    // hC22->Draw("same");
+    //hC22->Draw("same");
     // hCV422->Draw("same");
     // hCLinear->Draw("same");
     
-	int binning = 20;
-    //int binning = 40;
-    double errorNum;
-    double sumNum = 0;
-    double sumDenom = 1;
-    double content = 0;
+	// int binning = 20;
+    // //int binning = 40;
+    // double errorNum;
+    // double sumNum = 0;
+    // double sumDenom = 1;
+    // double content = 0;
 
-    if(binning == 20)RebinAll(hC22, hC22_Rebin, 20);
-    if(!hC22) fPrint("Warning there is no histogram");
-    //hC22_Rebin->GetXaxis()->SetRangeUser(10, 110);
-    //hC22_Rebin->Draw("same");
+    // if(binning == 20)RebinAll(hC22, hC22_Rebin, 20);
+    // if(!hC22) fPrint("Warning there is no histogram");
+    // //hC22_Rebin->GetXaxis()->SetRangeUser(10, 110);
+    // //hC22_Rebin->Draw("same");
 
-    if(binning == 20)RebinAll(hCV422, hCV422_Rebin, 20);
-    if(!hCV422) fPrint("Warning there is no histogram");
-    //hCV422_Rebin->GetXaxis()->SetRangeUser(10, 110);
-    //hCV422_Rebin->Draw("same");
+    // if(binning == 20)RebinAll(hCV422, hCV422_Rebin, 20);
+    // if(!hCV422) fPrint("Warning there is no histogram");
+    // //hCV422_Rebin->GetXaxis()->SetRangeUser(10, 110);
+    // //hCV422_Rebin->Draw("same");
 
-    if(binning == 20)RebinAll(hCLinear, hCLinear_Rebin, 20);
-    if(!hCLinear) fPrint("Warning there is no histogram");
+    // if(binning == 20)RebinAll(hCLinear, hCLinear_Rebin, 20);
+    //if(!hCLinear) fPrint("Warning there is no histogram");
     //hCLinear_Rebin->GetXaxis()->SetRangeUser(10, 110);
     //hCLinear_Rebin->Draw("same");
 
@@ -160,39 +206,39 @@ void All_Gaps_Rebin_V4_V5_NL_L()
     //..v_{4,22}
     //hC22_Rebin->GetYaxis()->SetRangeUser(0., 4.);
     //hC22_Rebin->GetXaxis()->SetRangeUser(0., 140);
-    hC22_Rebin->SetMarkerSize(1.);
-    hC22_Rebin->Draw("same");
-    hC22_Rebin->SetTitle("Non-Linear Response v_{4,22} for pPb at #sqrt{s_{NN}} = 5.02 TeV");
-    hC22_Rebin->SetMarkerStyle(kFullTriangleUp); 
-    hC22_Rebin->SetMarkerColor(kRed+1);
-    hC22_Rebin->GetYaxis()->SetTitleOffset(1.4);
-    //hC22_Rebin->GetYaxis()->CenterTitle(true);
-    hC22_Rebin->GetYaxis()->SetTitle("v_{4,22}");
+    // hC22_Rebin->SetMarkerSize(1.);
+    // hC22_Rebin->Draw("same");
+    // hC22_Rebin->SetTitle("Non-Linear Response v_{4,22} for pPb at #sqrt{s_{NN}} = 5.02 TeV");
+    // hC22_Rebin->SetMarkerStyle(kFullTriangleUp); 
+    // hC22_Rebin->SetMarkerColor(kRed+1);
+    // hC22_Rebin->GetYaxis()->SetTitleOffset(1.4);
+    // //hC22_Rebin->GetYaxis()->CenterTitle(true);
+    // hC22_Rebin->GetYaxis()->SetTitle("v_{4,22}");
 
-    hC22_Rebin->GetXaxis()->SetTitleOffset(1.4);
-    //hC22_Rebin->GetXaxis()->CenterTitle(true);
-    gStyle->SetTitleFontSize(0.2);
-    hC22_Rebin->GetXaxis()->SetTitle("N_{Ch}(|#Delta#eta| < 0.8)");
-    hC22_Rebin->SetFillColor(kRed-9);
-    c->Update();
+    // hC22_Rebin->GetXaxis()->SetTitleOffset(1.4);
+    // //hC22_Rebin->GetXaxis()->CenterTitle(true);
+    // gStyle->SetTitleFontSize(0.2);
+    // hC22_Rebin->GetXaxis()->SetTitle("N_{Ch}(|#Delta#eta| < 0.8)");
+    // hC22_Rebin->SetFillColor(kRed-9);
+    // c->Update();
     
-    TLatex latex;
-    latex.SetTextSize(0.035);
-    latex.SetTextAlign(13);  //align at top
-    latex.DrawLatex(10,3.5,"#sqrt{s_{NN}} = 5.02 TeV");
+    // TLatex latex;
+    // latex.SetTextSize(0.035);
+    // latex.SetTextAlign(13);  //align at top
+    // latex.DrawLatex(10,3.5,"#sqrt{s_{NN}} = 5.02 TeV");
 
-    latex.SetTextAlign(13);  //centered
-    latex.DrawLatex(10,3.3,"|#Delta #eta| < 0.8");
+    // latex.SetTextAlign(13);  //centered
+    // latex.DrawLatex(10,3.3,"|#Delta #eta| < 0.8");
 
-    latex.SetTextAlign(13);  //centered
-    latex.DrawLatex(10,3.1," 0.2 < p_{T} < 3.0");
+    // latex.SetTextAlign(13);  //centered
+    // latex.DrawLatex(10,3.1," 0.2 < p_{T} < 3.0");
 
-    TLegend *leg = new TLegend(0.4, 0.5, 0.55, 0.55); 
-    leg->AddEntry(hC22_Rebin, "v_{4,22}", "fp");
-    leg->Draw("same");
+    // TLegend *leg = new TLegend(0.4, 0.5, 0.55, 0.55); 
+    // leg->AddEntry(hC22_Rebin, "v_{4,22}", "fp");
+    // leg->Draw("same");
 
-    leg->SetBorderSize(0);
-    gStyle->SetTitleFontSize(0.2);
+    // leg->SetBorderSize(0);
+    // gStyle->SetTitleFontSize(0.2);
 
 
 
@@ -327,84 +373,9 @@ void All_Gaps_Rebin_V4_V5_NL_L()
     
 //     leg->SetBorderSize(0);
 //     gStyle->SetTitleFontSize(0.2);
-
-//     //***************************************************************************
-//     //..Writting to the Root.file:
-//     //..V4,22
-//     // TFile* fileOutput = new TFile("Rebin_Plots_V4_Linear_NonLinear_Response_Gap02.root","RECREATE");
-//     // if(!fileOutput) return;
-//     // fileOutput->cd();
-//     // hC22_Rebin->Write();
-//     // hCV422_Rebin->Write();
-//     // hCLinear_Rebin->Write();
-//     // fileOutput->Close();
-//     // return;
-
-//     //..V5,32
-//     // TFile* fileOutput = new TFile("Rebin_Plots_V5_Linear_NonLinear_Response_Gap02.root","RECREATE");
-//     // if(!fileOutput) return;
-//     // fileOutput->cd();
-//     // hC22_Rebin->Write();
-//     // hCV532_Rebin->Write();
-//     // hCLinear_Rebin->Write();
-//     // fileOutput->Close();
-//     // return;
-
-//     //..Chi4,22 and Chi5,32
-//     TFile* fileOutput = new TFile("Rebin_Plots_Chi422_Chi532_Gap02.root","RECREATE");
-//     if(!fileOutput) return;
-//     fileOutput->cd();
-//     hChi422_Rebin->Write();
-//     hChi532_Rebin->Write();
-//     fileOutput->Close();
-//     return;
 }
 
 // //..Define Rebin Function
-// //_____________________________________________________________________________
-// void RebinAll(TH1D *h1, TH1D *h2, int binning)
-// {
-	
-// 	double sumNum[200] = {0};
-// 	double sumDenom[200] = {0};
-// 	double errorNum[200] = {0};
-// 	double content[200] = {0};
-// 	double error[200] = {0};
-	
-// 	int bin = 1;
-
-// 	if(binning == 10)
-// 	{
-// 		for(int j=1; j<201; j=j+10)
-// 		{
-// 			for(int i=0; i<10; i++)
-// 			{
-// 				if(h1->GetBinContent(j+i) != 0)
-// 				{
-// 					errorNum[bin] += TMath::Power(h1->GetBinError(j+i), 2.);
-// 					sumNum[bin] += h1->GetBinContent(j+i);
-// 					sumDenom[bin] += 1;
-// 				}
-//                 cout << "i: " << i << endl;
-//                 cout << "sumNum: " << sumNum[i] << endl;
-//                 cout << "errorNum: " << errorNum[i] << endl;
-// 			}
-// 			bin += 1;
-// 		}
-
-// 		for(int i=1; i<21; i++)
-// 		{
-// 			if(sumDenom[i] != 0) content[i] = sumNum[i]/sumDenom[i];
-// 			if(sumDenom[i] != 0) error[i] = TMath::Sqrt(errorNum[i]/(TMath::Power(sumDenom[i], 2.)));
-//             //if(sumDenom[i] != 0) error[i] = TMath::Sqrt(errorNum[i] * 1/ (TMath::Power(sumDenom[i], 2.)));
-//             cout << "i: " << i << endl;
-//             cout << "error: " << error[i] << endl;
-// 			h2->SetBinContent(i, content[i]);
-// 			h2->SetBinError(i, error[i]);
-// 		}
-// 	}
-
-// }
 // //_______________________________________________________________________________
 void RebinAll(TH1D *h1, TH1D *h2, int binning)
 {
