e622d4f72c79caf2ec3c10ed3a4ae24d414ad22c
Helene Ausar <helene.ausar@cern.ch>
2018-02-02 17:24:28 +0100

    Get the first result for delta eta dependent 3 and 4particle

--- Delta_eta_dependent_Three_Four_partcorr.cxx
+++ Delta_eta_dependent_Three_Four_partcorr.cxx
@@ -1,4 +1,4 @@
-//Macro To Calculate the Final results for plot vs. Delta_EtaGap:
+//Macro To Calculate the Final results for  <<3>> and <<4>> plot vs. Delta_EtaGap to study the correlations instead of observables:
 //Root file:
 #include <TFile.h>
 #include <TMath.h>
@@ -11,29 +11,22 @@
 using namespace std; 
 
 void Delta_eta_dependent_Three_Four_partcorr() {
-    double Bin_value_v422;
-    double Bin_value_hC22;
-    double Bin_value_hCLinear;
+
 
     TCanvas *c = new TCanvas("c","The Test HISTOGRAM ", 100, 8, 700, 600);
     c->SetFillColor(10);
     c->cd();
 
-
-    //..Root.file for all Eta-dependant plots
-    // TFile* fileOutput = new TFile("/Users/Helena/Desktop/Helen_simpletask/ErrorPropagations/Eta_Dependent/NewCondition_Delta_eta_dependent_V4L_NL422_V4_2part.root","RECREATE");
-
-    TFile* fileOutput2 = new TFile(,"RECREATE");
+    //..Create New file.root Eta-dependant of <<3>> and <<4>> plots
+    TFile* fileOutput = new TFile("/Users/Helena/Desktop/Helen_simpletask/ErrorPropagations/Eta_Dependent/New_Delta_eta_dependent_3Part_4Part/New_Delta_eta_dependent_3part_4part_correlation.root","RECREATE");
 
     int n= 9;
-    double EtaGap[9]= {0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8};
+    double EtaGap[9]= {0, 1, 2, 3, 4, 5, 6, 7, 8};
     double y1[9] = {};
     double y2[9] = {};
     double y3[9] = {};
-    //double hC22 [9]= {0.0301489, 0.0275432, 0.0242499, 0.0211225, 0.0185883, 0.0169007, 0.0152636, 0.0138636, 0.0127156};
-    //double hCV422 [9] = {0.0171917, 0.0157672, 0.0144088, 0.0136457, 0.0136395, 0.0125566, 0.0135752, 0.0346021, 0.0334225};
-    //double hCLinear [9] = {0.0263081, 0.0226898, 0.0203149, 0.0186658, 0.0160822, 0.0134485, 0.0136349, 0.0118987, 0.0118619};
-    
+    double y4[9] = {};
+
     //**************************************************************************
     //TEST:
     // TGraph* gr = new TGraph(n, EtaGap, hC22);
@@ -84,48 +77,54 @@ void Delta_eta_dependent_Three_Four_partcorr() {
     //gr->Draw("ACP");
     //gr->Draw("AC*");
     //**************************************************************************
-    // TFile* file = TFile::Open(
-    // "/Users/Helena/Desktop/Helen_simpletask/ErrorPropagations/AllGaps_Linear_NL_V4_V5/AllGaps_Rebin_NewCondition_V4/Rebin_V4_Linear_NL_histograms_NewCondition.root", "READ");
+    TFile* file = TFile::Open(
+    "/Users/Helena/Desktop/Helen_simpletask/ErrorPropagations/Cut_RawData_3particle_4particle/New_Rebin_Cut_Data_3_4_particle/New_Rebin_Cut_Data_3part_4part_histograms.root", "READ");
 
-    TFile* file = TFile::Open("/Users/Helena/Desktop/bar_GF/Helene_WNUA/merging/LHC16q_CENT_SDD_0Gap_2_08Gap_FullGaps.root", "READ");
-
-    for(int i=1; i<11; i++) {
+    for(int i=1; i < 6; i++) {
         for(int gap=0; gap < 9; gap++) {
             
-            // //..V4:
-            // TString foo_22(TString::Format("hC22_Rebin_Gap0%0d", gap));
-            // TString foo_NL422(TString::Format("hCV422_Rebin_Gap0%0d", gap));
-            // TString foo_LV4(TString::Format("hCLinear_Rebin_Gap0%0d", gap));
+            //..<<3>>_Rebin and <<4>>_Rebin:
+            TString foo_34(TString::Format("hC34_Rebin_Gap0%0d", gap));
+            TString foo_35(TString::Format("hC35_Rebin_Gap0%0d", gap));
+            TString foo_42(TString::Format("hC42_Rebin_Gap0%0d", gap));
+            TString foo_43(TString::Format("hC43_Rebin_Gap0%0d", gap));
+
+            //..<<3>>_Rebin and <<4>>_Rebin:
+            TH1D *hC34_gap = (TH1D*)file->Get(foo_34);
+            TH1D *hC35_gap = (TH1D*)file->Get(foo_35);
+            TH1D *hC42_gap = (TH1D*)file->Get(foo_42);
+            TH1D *hC43_gap = (TH1D*)file->Get(foo_43);
             
             // //..V4:
-            // TH1D *hC22_gap = (TH1D*)file->Get(foo_22);
-            // TH1D *hCV422_gap = (TH1D*)file->Get(foo_NL422);
-            // TH1D *hCLinear_gap = (TH1D*)file->Get(foo_LV4);
-            
-            // //..V4:
-            // y1[gap] = hC22_gap->GetBinContent(i);
-            // y2[gap] = hCV422_gap->GetBinContent(i);
-            // y3[gap] = hCLinear_gap->GetBinContent(i);
+            y1[gap] = hC34_gap->GetBinContent(i);   //??? for i loop?
+            y2[gap] = hC35_gap->GetBinContent(i);
+            y3[gap] = hC42_gap->GetBinContent(i);
+            y4[gap] = hC43_gap->GetBinContent(i);
 
         
             //..V5:
-            TString foo_25(TString::Format("hC25_Rebin_Gap0%0d", gap));
-            TString foo_NL532(TString::Format("hCV532_Rebin_Gap0%0d", gap));
-            TString foo_LV5(TString::Format("hCLinearV5_Rebin_Gap0%0d", gap));
+            // TString foo_25(TString::Format("hC25_Rebin_Gap0%0d", gap));
+            // TString foo_NL532(TString::Format("hCV532_Rebin_Gap0%0d", gap));
+            // TString foo_LV5(TString::Format("hCLinearV5_Rebin_Gap0%0d", gap));
             
-            //..V5:
-            TH1D *hC25_gap = (TH1D*)file->Get(foo_25);
-            TH1D *hCV532_gap = (TH1D*)file->Get(foo_NL532);
-            TH1D *hCLinearV5_gap = (TH1D*)file->Get(foo_LV5);
+            // //..V5:
+            // TH1D *hC25_gap = (TH1D*)file->Get(foo_25);
+            // TH1D *hCV532_gap = (TH1D*)file->Get(foo_NL532);
+            // TH1D *hCLinearV5_gap = (TH1D*)file->Get(foo_LV5);
             
-            y1[gap] = hC25_gap->GetBinContent(i);
-            y2[gap] = hCV532_gap->GetBinContent(i);
-            y3[gap] = hCLinearV5_gap->GetBinContent(i);
-
-            cout << "y: " << y1[gap] << endl;
+            // y1[gap] = hC25_gap->GetBinContent(i); 
+            // y2[gap] = hCV532_gap->GetBinContent(i);
+            // y3[gap] = hCLinearV5_gap->GetBinContent(i);
+            cout << "*********************************************" << endl;
+            cout << "y1: " << y1[gap] << endl;
+            cout << "y2: " << y2[gap] << endl;
+            cout << "y3: " << y3[gap] << endl;
+            cout << "y4: " << y4[gap] << endl;
             cout << "i: " << i <<endl;
             cout << "Gap: " << gap << endl;
-            
+            nbin_range = hC34_gap->GetXaxis()->FindBin(106) - hC34_gap->GetXaxis()->FindBin(5);
+            cout << "Number of bins: " << nbin_range << endl; 
+            cout << "**********************************************" << endl;
             //*********************************************************************
             // //..V4
             // TGraph* gr1 = new TGraph(n, EtaGap, y1);
@@ -174,9 +173,9 @@ void Delta_eta_dependent_Three_Four_partcorr() {
             gr1->SetLineWidth(4);
             gr1->SetMarkerColor(4);
             gr1->SetMarkerStyle(21);
-            gr1->SetTitle(TString::Format("v_{5}{2} 2-Particle correlations_Bin%0d", i));
+            gr1->SetTitle(TString::Format("<<3>>_{4|-2,-2} 3-Particle correlations, Bin%0d", i));
             gr1->GetXaxis()->SetTitle("#Delta #eta");
-            gr1->GetYaxis()->SetTitle("v_{5}{2}");
+            gr1->GetYaxis()->SetTitle("<<3>>_{4|-2,-2}");
             gr1->GetYaxis()->SetTitleOffset(1.4);
             gr1->Draw("ACP");
             //gr1->Draw("AC*");
@@ -186,9 +185,9 @@ void Delta_eta_dependent_Three_Four_partcorr() {
             gr2->SetLineWidth(4);
             gr2->SetMarkerColor(4);
             gr2->SetMarkerStyle(21);
-            gr2->SetTitle(Form("v_{5,32} Non-Linear Response, Bin%0d", i));
+            gr2->SetTitle(Form("<<3>>_{5|-3,-2} 3-Particle correlations, Bin%0d", i));
             gr2->GetXaxis()->SetTitle("#Delta #eta");
-            gr2->GetYaxis()->SetTitle("v_{5,32}");
+            gr2->GetYaxis()->SetTitle("<<3>>_{5|-3,-2}");
             gr2->GetYaxis()->SetTitleOffset(1.4);
             gr2->Draw("ACP");
             //gr2->Draw("AC*");
@@ -199,27 +198,38 @@ void Delta_eta_dependent_Three_Four_partcorr() {
             gr3->SetLineWidth(4);
             gr3->SetMarkerColor(4);
             gr3->SetMarkerStyle(21);
-            gr3->SetTitle(Form("v_{5}^{L} Linear Response, Bin%0d", i));
+            gr3->SetTitle(Form("<<4>>_{2,2|-2,-2} 4-Particle correlations, Bin%0d", i));
             gr3->GetXaxis()->SetTitle("#Delta #eta");
-            gr3->GetYaxis()->SetTitle("v_{5}^{L}");
+            gr3->GetYaxis()->SetTitle("<<4>>_{2,2|-2,-2}");
             gr3->GetYaxis()->SetTitleOffset(1.4);
             gr3->Draw("ACP");
             //gr3->Draw("AC*");
 
-            
-            
+            TGraph* gr4 = new TGraph(n, EtaGap, y4);
+            gr4->SetLineColor(2);
+            gr4->SetLineWidth(4);
+            gr4->SetMarkerColor(4);
+            gr4->SetMarkerStyle(21);
+            gr4->SetTitle(Form("<<4>>_{2,3|-2,-3} 4-Particle correlations, Bin%0d", i));
+            gr4->GetXaxis()->SetTitle("#Delta #eta");
+            gr4->GetYaxis()->SetTitle("<<4>>_{2,2|-2,-2}");
+            gr4->GetYaxis()->SetTitleOffset(1.4);
+            gr4->Draw("ACP");
+            //gr4->Draw("AC*");
+   
         }
-        // if(!fileOutput) return;
-        // fileOutput->cd();
-        // gr1->Write(Form("EtaDependent_V4_2particle_plots_Bin%0d", i));
-        // gr2->Write(Form("EtaDependent_V4_NL422_plots_Bin%0d", i));
-        // gr3->Write(Form("EtaDependent_V4_Linear_plots_Bin%0d", i));
+        if(!fileOutput) return;
+        fileOutput->cd();
+        gr1->Write(Form("<<3>>_{4|-2,-2} 3-Particle correlations, Bin%0d", i));
+        gr2->Write(Form("<<3>>_{5|-3,-2} 3-Particle correlations, Bin%0d", i));
+        gr3->Write(Form("<<4>>_{2,2|-2,-2} 4-Particle correlations, Bin%0d", i));
+        gr4->Write(Form("<<4>>_{2,3|-2,-3} 4-Particle correlations, Bin%0d", i));
         
-        if(!fileOutput2) return;
-        fileOutput2->cd();
-        gr1->Write(Form("EtaDependent_V5_2particle_plots_Bin%0d", i));
-        gr2->Write(Form("EtaDependent_V5_NL532_plots_Bin%0d", i));
-        gr3->Write(Form("EtaDependent_V5_Linear_plots_Bin%0d", i));
+        // if(!fileOutput2) return;
+        // fileOutput2->cd();
+        // gr1->Write(Form("EtaDependent_V5_2particle_plots_Bin%0d", i));
+        // gr2->Write(Form("EtaDependent_V5_NL532_plots_Bin%0d", i));
+        // gr3->Write(Form("EtaDependent_V5_Linear_plots_Bin%0d", i));
         //c->SaveAs("/Users/Helena/Desktop/Plots/Delta_eta_plots/test.png");
         cout << "i finished: " << i <<endl;
         //return;
