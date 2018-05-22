e28a7aa2d4bbf6ba4d65ff5566e63bca03bd4433
Helene Ausar <helene.ausar@cern.ch>
2018-05-22 17:31:27 +0200

    Ratio for MC + Data made

--- Rebin_MC_Data/Ratio_MC_Data.C
+++ Rebin_MC_Data/Ratio_MC_Data.C
@@ -7,14 +7,14 @@ void Ratio_MC_Data() {
 //******************************************************************************
     //=============================================================================================
     //Open the Rebin Data:
-    TFile *file1 = TFile::Open("/Users/Helena/Desktop/Helen_simpletask/ErrorPropagations/Rebin_MC_Data/Results_Rebin_Data.root","READ");
-    TH1D* h1 = (TH1D*)file1->Get("Rebin_Cn35_Gap08");
-    TH1D* h2 = (TH1D*)file1->Get("Rebin_Cn35_Gap00");
+    // TFile *file1 = TFile::Open("/Users/Helena/Desktop/Helen_simpletask/ErrorPropagations/Rebin_MC_Data/Results_Rebin_Data.root","READ");
+    // TH1D* h1 = (TH1D*)file1->Get("Rebin_Cn25_Gap08");
+    // TH1D* h2 = (TH1D*)file1->Get("Rebin_Cn25_Gap00");
 
-    //Open MC data:
+    // //Open MC data:
     TFile *file2 = TFile::Open("/Users/Helena/Desktop/Helen_simpletask/ErrorPropagations/Rebin_MC_Data/Rebin_MC.root","READ");
-    TH1D* h1 = (TH1D*)file2->Get("MC_Rebin_Cn35_Gap08");//Open root.file:
-    TH1D* h2 = (TH1D*)file2 ->Get("MC_Rebin_Cn35_Gap00");
+    TH1D* h1 = (TH1D*)file2->Get("MC_Rebin_Cn43_Gap08");//Open root.file:
+    TH1D* h2 = (TH1D*)file2 ->Get("MC_Rebin_Cn43_Gap00");
 
 //***************************************************************************
     // Define the Canvas
@@ -22,13 +22,14 @@ void Ratio_MC_Data() {
     //***********h1***************
     h1->SetStats(0);          // No statistics on upper plot
     h1->SetTitle(0);          // No Title on the upper plot
-    h1->SetMinimum(1e4);
-    h1->SetMaximum(40e6);
+    //h1->SetMinimum(1e4);
+    //h1->SetMaximum(40e6);
     h1->SetMarkerStyle(kFullCircle); 
     h1->SetMarkerColor(kBlue+1);
     h1->SetStats(0);
     h1->GetYaxis()->SetTitleOffset(1.6);
-    //h1->GetXaxis()->SetRangeUser(-0.8, 0.8); // Only for Eta 
+    h1->GetXaxis()->SetTitle("N_{Ch} (|#eta| < 0.8)");
+    h1->GetXaxis()->SetRangeUser(20, 90); // Only for Eta 
     //h1->Draw("e1");               // Draw h1
     
 
@@ -38,32 +39,32 @@ void Ratio_MC_Data() {
     h2->SetLineColor(kRed);
     h2->SetLineWidth(2);
     h2->SetStats(0);
+    h2->GetXaxis()->SetRangeUser(20, 90); // Only for Eta 
     //h2->Draw("same");         // Draw h2 on top of h1
 
 
     // // Define the ratio plot
     TH1D *h3 = (TH1D*)h1->Clone("h3");
     h3->SetLineColor(kRed);   //kBlack
-    h3->SetMinimum(0.0);  // Define Y ..
-    h3->SetMaximum(2.); // .. range
+    h3->SetMinimum(-70);  // Define Y ..
+    h3->SetMaximum(50); // .. range
     h3->Sumw2();
     h3->SetStats(0);      // No statistics on lower plot
     h3->Divide(h2);
-    h3->SetMarkerStyle(kOpenDiamond);
-    h3->SetMarkerSize(2);
-    h3->SetMarkerColor(kMagenta+1);
-    h3->SetLineColor(kMagenta+1);
+    h3->SetMarkerStyle(kFullCircle);
+    h3->SetMarkerSize(1.5);
+    h3->SetMarkerColor(kRed+1);
+    h3->SetLineColor(kRed+1);
     h3->Draw("ep");       // Draw the ratio plot
 
     // // h1 settings:==============:
-    h1->SetLineColor(kBlue+1);
+    h1->SetLineColor(kRed+1);
     h1->SetLineWidth(2);
 
     // // Y axis h1 plot settings
     h1->GetYaxis()->SetTitleSize(20);
     h1->GetYaxis()->SetTitleFont(43);
     //h1->GetXaxis()->SetTitle("p_{T}(GeV/c)");
-    h1->GetXaxis()->SetTitle("#eta");
     //h1->GetXaxis()->SetTitle("V_{z}(cm)");
     //h1->GetYaxis()->SetTitle("Counts");
     
@@ -76,7 +77,7 @@ void Ratio_MC_Data() {
     h3->SetTitle(0); // Remove the ratio title
 
     // Y axis ratio plot settings:=============:
-    h3->GetYaxis()->SetTitle("Efficiency");
+    h3->GetYaxis()->SetTitle("C_{23}{4}");
     //h3->SetTitle("; p_{T}; Efficiency");  Another way of doing that: first one is title, xaxis, yaxis
     h3->GetYaxis()->SetNdivisions(505);
     h3->GetYaxis()->SetTitleSize(20);
@@ -108,13 +109,13 @@ void Ratio_MC_Data() {
     
     //x1, y1, x2, y2
     //TLegend *leg = new TLegend(0.56,0.75,0.89,0.9);
-    TLegend *leg = new TLegend(0.36,0.6,0.75,0.8);  //Make a good position
+    TLegend *leg = new TLegend(0.35,0.2,0.75,0.4);  //Make a good position
     //leg->AddEntry(h1,"MC-Reco", "ep");
     //leg->AddEntry(h2,"MC-Truth", "ep");
-    leg->AddEntry(h3, "#frac{MC-Reco}{MC-Truth}", "ep");
+    leg->AddEntry(h3, "#frac{MC-#Delta#eta-0.8}{MC-#Delta#eta-0.0}", "ep");
     //of MC-Reco to MC-Truth
     leg->Draw();
     //h3->Draw("ep");       // Draw the ratio plot
     // Save in Pic Folder:======================:
-    c->SaveAs("/Users/Helena/Dropbox/0-Speciale/code/panda/Sand_boks/Old/pic/Plots_NUE/Ratio_MCReco_MCTruth_pTX_Projection.png");
+    // c->SaveAs("/Users/Helena/Dropbox/0-Speciale/code/panda/Sand_boks/Old/pic/Plots_MC/Ratio_Plots_MC_Data_Smallest_Largest/Ratio_MC_C43_Gap00_Gap08.png");
     }
